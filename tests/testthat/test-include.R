dc4596c353a4d315a7e7c559db668fe33783c575
Jeff Allen <cran@trestletech.com>
2017-07-24 20:50:56 +0000

    version 0.4.2

--- tests/testthat/test-include.R
+++ tests/testthat/test-include.R
@@ -1,10 +1,4 @@
-make_req <- function(verb, path){
-  req <- new.env()
-  req$REQUEST_METHOD <- toupper(verb)
-  req$PATH_INFO <- path
-  req$rook.input <- list(read_lines = function(){ "" })
-  req
-}
+context("Includes")
 
 test_that("Includes work", {
   r <- plumber$new("files/includes.R")
--- tests/testthat/test-response.R
+++ tests/testthat/test-response.R
@@ -1,3 +1,5 @@
+context("Responses")
+
 test_that("response properly sets basic cookies", {
   res <- PlumberResponse$new()
   res$setCookie("abc", "two words")
@@ -36,9 +38,3 @@ test_that("can set multiple same-named headers", {
   expect_true(another)
 })
 
-test_that("doesn't overwrite CORS", {
-  res <- PlumberResponse$new()
-  res$setHeader("Access-Control-Allow-Origin", "originhere")
-  head <- res$toResponse()$headers
-  expect_equal(head[["Access-Control-Allow-Origin"]], "originhere")
-})
